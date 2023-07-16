c696ad08a22e4f055261493bdbbf782fa29c084c
favreje <favreje@gmail.com>
2023-07-16 11:42:09 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Author:      Jeffrey Favret
+// Date:        07/03/23
+// Project:     Basic tutorial - a basic understanding of web development
+
+
+function showMessage() {
+    const msg = 'Thank you for registering with us! We are excited to see you take ' + 
+        'your software development skills to the next level.\r\rPlease respond to ' +
+        'your confirmation email so that we can get you started right away.\r\r' +
+        'Welcome aboard!';
+    alert(msg);
+
+}
